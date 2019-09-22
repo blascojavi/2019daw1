3e3fc1a7cfebf9bef0ab395db54525c49b4864bd
AntonioFl <buceocv@gmail.com>
2019-09-22 21:29:45 +0200

    Terminado ejercicios tema 1 programacion

---
+++ Programacion/TEMA 1/src/CalculadoraLibrasEuros.java
@@ -0,0 +1,29 @@
+/*
+* 4)Dada un cantidad correspondiente a euros, calcule su equivalente en libras esterlinas, sabiendo que
+*  1 libra son 1.18 euros. Define una variable constante para el cambio.
+* Muestra por pantalla el calculo final.
+*
+*/
+
+import java.util.Scanner;
+
+public class CalculadoraLibrasEuros {
+
+    public static void main (String[] args ){
+        final double LIBRA_EURO = 1.18;
+
+        Scanner scanEuro = new Scanner(System.in);
+        System.out.println("Introduzca cantidad de Libras a convertir");
+
+        double libraIntro = scanEuro.nextDouble();
+
+        System.out.println("Ha introducido : " + libraIntro + " El valor de libra a Euro es de : " + LIBRA_EURO);
+
+        String result = String.format("%.2f" , libraIntro * LIBRA_EURO);
+
+        System.out.println("RESULTADO : "  + result + " Euros");
+
+
+    }
+
+}
